9dfd88bacb85b0f7d398fda65134896fa620e84f
martinseifertprojuventute <martin.seifert@projuventute.ch>
2023-03-16 11:54:45 +0100

    added console to core (tmp!)

--- raisenow/widget_core.js
+++ raisenow/widget_core.js
@@ -1,3 +1,5 @@
+window.console.log('     widget core start');
+
 var widgetContainer = document.getElementsByClassName("rnw-widget-container")[0];
 
 // add the widget core
@@ -5,3 +7,5 @@ var widgetCore = document.createElement("script");
 widgetCore.setAttribute("src", "https://tamaro.raisenow.com/projuventute/latest/widget.js");
 widgetCore.setAttribute("defer", ""); // script is added after DOM-ready => to execute it, defer!
 widgetContainer.append(widgetCore);
+
+window.console.log('     widget core complete');
