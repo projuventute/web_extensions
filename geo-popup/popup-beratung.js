ae7a2349f94a91cb88504c1abdd85920ae730c5b
oliverplein <56489497+oliverplein@users.noreply.github.com>
2023-10-10 15:52:26 +0200

    translations added

--- geo-popup/popup-beratung.js
+++ geo-popup/popup-beratung.js
@@ -1,4 +1,16 @@
 function Popup(site, geo) {
+  let linkTitel =
+    geo === "DE"
+      ? "Anlaufstellen in deinem Land"
+      : geo === "AT"
+      ? "Anlaufstellen in deinem Land"
+      : geo === "FR"
+      ? site === "147"
+        ? "Ressources dans ton pays"
+        : "Ressources dans votre pays"
+      : geo === "IT"
+      ? "Per conoscere a chi rivolgersi nel tuo Paese"
+      : "";
   let links =
     geo === "DE"
       ? [
@@ -21,9 +33,37 @@ function Popup(site, geo) {
         ]
       : geo === "FR"
       ? [
+          { titel: "119", link: "https://www.allo119.gouv.fr/" },
+          {
+            titel: "En cas d’idées suicidaires, appelle le 3114",
+            link: "https://3114.fr/",
+          },
           {
             titel: "Les lignes d’écoute – Psycom – Santé Mentale Info",
             link: "https://www.psycom.org/sorienter/les-lignes-decoute/",
+          },
+        ]
+      : geo === "IT"
+      ? [
+          {
+            titel: "Telefono Azzurro (19696 - linea di ascolto 24h su 24)",
+            link: "https://www.azzurro.it/",
+          },
+          {
+            titel: "Telefono Amico (02 2327 2327)",
+            link: "tel:0223272327",
+          },
+          {
+            titel: "Telefono Amico WhatsApp (324 011 7252)",
+            link: "https://wa.link/ug93ne",
+          },
+          {
+            titel: "114 (numero per le emergenze gestito dal Telefono Azzurro",
+            link: "tel:114",
+          },
+          {
+            titel: "116 000 (numero unico europeo per minori scomparsi",
+            link: "tel:116000",
           },
         ]
       : [];
@@ -34,9 +74,19 @@ function Popup(site, geo) {
       : geo === "AT"
       ? "Österreich"
       : geo === "FR"
-      ? "Frankreich"
+      ? "France"
       : geo === "IT"
       ? "Italia"
+      : "";
+  const greeting =
+    geo === "DE"
+      ? "Hallo"
+      : geo === "AT"
+      ? "Hallo"
+      : geo === "FR"
+      ? "Bonjour"
+      : geo === "IT"
+      ? "Ciao"
       : "";
   const geoTextDe =
     site === "147"
@@ -44,12 +94,24 @@ function Popup(site, geo) {
       : `Es sieht so aus als würden Sie aus ${land} auf projuventute.ch zugreifen.`;
   const infoTextDe =
     site === "147"
-      ? `Leider ist unser Beratungsangebot ausschließlich für Kinder und Jugendliche aus der Schweiz verfügbar.`
-      : `Leider ist unser Beratungsangebot ausschließlich für Eltern und Bezugspersonen von Kindern und Jugendliche aus der Schweiz verfügbar.`;
-  const geoTextFr = site === "147" ? `` : ``;
-  const infoTextFr = site === "147" ? `` : ``;
-  const geoTextIt = site === "147" ? `` : ``;
-  const infoTextIt = site === "147" ? `` : ``;
+      ? `Leider ist unser Beratungsangebot ausschließlich für Kinder und Jugendliche aus der Schweiz und Fürstentum Liechtenstein verfügbar.`
+      : `Leider ist unser Beratungsangebot ausschließlich für Eltern und Bezugspersonen von Kindern und Jugendliche aus der Schweiz und Fürstentum Liechtenstein verfügbar.`;
+  const geoTextFr =
+    site === "147"
+      ? `Il semblerait que tu contactes le 147.ch depuis la ${land}`
+      : `Il semblerait que vous nous contactiez depuis la ${land}`;
+  const infoTextFr =
+    site === "147"
+      ? `Notre service de conseil n’est destiné qu’aux enfants et aux jeunes qui vivent en Suisse.`
+      : `Notre service de conseils aux parents est destiné aux familles vivant en Suisse exclusivement.`;
+  const geoTextIt =
+    site === "147"
+      ? `Sembra che tu stia accedendo a 147.ch dall'${land}. `
+      : `Sembra che lei stia accedendo a projuventute.ch dall'${land}.`;
+  const infoTextIt =
+    site === "147"
+      ? `Purtroppo il nostro servizio di consulenza è disponibile solo per i bambini e i giovani svizzeri. `
+      : `Purtroppo il nostro servizio di consulenza è disponibile solo per i genitori e chi si occupa di bambini e adolescenti della Svizzera. `;
 
   const geoText =
     geo === "DE"
@@ -71,17 +133,37 @@ function Popup(site, geo) {
       : geo === "IT"
       ? infoTextIt
       : "";
+  const weiterText =
+    geo === "DE"
+      ? "Weiter zu"
+      : geo === "AT"
+      ? "Weiter zu"
+      : geo === "FR"
+      ? "Continuer sur le"
+      : geo === "IT"
+      ? "IT: Vada su"
+      : "";
 
   let linksHtml = "";
   for (let i = 0; i < links.length; i++) {
     linksHtml += `<div style="display:flex"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" > <path d="M7 12H17" stroke="#CD679D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> <path d="M13 8L17 12L13 16" stroke="#CD679D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> </svg> <a href="${links[i].link}" target="_blank" style="color:black">${links[i].titel}</a></div>`;
   }
-  let html = `<div id="geo-popup-pj" style="position: fixed;z-index:10001;width: 95%; min-width: 320px; max-width: 650px; background-color: white; padding: 40px; font-family: Montserrat, Arial, Helvetica, sans-serif; left: 50%; top: 50%; transform: translate(-50%, -50%); " > <h2 style="font-size: 56px">Hallo</h2> <p>${geoText}</p><p>${infoText}</p><h3 style="font-size: 24px">Anlaufstellen in deinem Land</h3>
+  let html = `<div id="geo-popup-pj" style="display: block; position: fixed;z-index:10001;width: 95%; min-width: 320px; max-width: 650px; background-color: white; padding: 40px; font-family: Montserrat, Arial, Helvetica, sans-serif; left: 50%; top: 50%; transform: translate(-50%, -50%); " > <h2 style="font-size: 56px">${greeting}</h2> <p>${geoText}</p><p>${infoText}</p><h3 style="font-size: 24px">${linkTitel}</h3>
   ${linksHtml}
-  <div style="margin-top: 40px; border-bottom: 1px solid #CD679D; display: inline-block; cursor:pointer;" onClick="closePopup()">Weiter zu 147.ch</div> </div>
-  <div style="position:fixed;width:100%;height:100%;background-color:rgba(255,255,255,0.6);z-index:10000;"></div>
+  <div style="margin-top: 40px; border-bottom: 1px solid #CD679D; display: inline-block; cursor:pointer;" onClick="closePopup()">${weiterText} ${" "} ${
+    site === "147" ? "147.ch" : "projuventute.ch"
+  }</div> </div>
+  <div id="geo-popup-pj-overlay" style="display: block; position:fixed;width:100%;height:100%;background-color:rgba(255,255,255,0.6);z-index:10000;" onClick="closePopup()"></div>
   `;
   document.body.innerHTML += html;
 }
 
-Popup("147", "DE");
+// weiterText + " " + site === "147" ? "147.ch" : "projuventute.ch"
+
+// to do: closePopup()
+function closePopup() {
+  document.getElementById("geo-popup-pj").style.display = "none";
+  document.getElementById("geo-popup-pj-overlay").style.display = "none";
+}
+
+Popup("projuventute", "IT");
