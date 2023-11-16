5229b8aeee086f7e826d12f30288108833283cbd
oliverplein <56489497+oliverplein@users.noreply.github.com>
2023-11-16 10:16:42 +0100

    backdrop fixed

--- geo-popup/popup-beratung.js
+++ geo-popup/popup-beratung.js
@@ -148,7 +148,7 @@ function Popup(site, geo) {
   for (let i = 0; i < links.length; i++) {
     linksHtml += `<div style="display:flex"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" > <path d="M7 12H17" stroke="#CD679D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> <path d="M13 8L17 12L13 16" stroke="#CD679D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> </svg> <a href="${links[i].link}" target="_blank" style="color:black">${links[i].titel}</a></div>`;
   }
-  let html = `<div id="geo-popup-pj" style="display: block; position: fixed;z-index:10001;width: 95%; min-width: 320px; max-width: 650px; background-color: white; padding: 40px; font-family: Montserrat, Arial, Helvetica, sans-serif; left: 50%; top: 50%; transform: translate(-50%, -50%); " > <h2 style="font-size: 56px">${greeting}</h2> <p>${geoText}</p><p>${infoText}</p><h3 style="font-size: 24px">${linkTitel}</h3>
+  let html = `<div id="geo-popup-pj" style="display: block; position: fixed;z-index:10001; top:0; width: 95%; min-width: 320px; max-width: 650px; background-color: white; padding: 40px; font-family: Montserrat, Arial, Helvetica, sans-serif; left: 50%; top: 50%; transform: translate(-50%, -50%); " > <h2 style="font-size: 56px">${greeting}</h2> <p>${geoText}</p><p>${infoText}</p><h3 style="font-size: 24px">${linkTitel}</h3>
   ${linksHtml}
   <div style="margin-top: 40px; border-bottom: 1px solid #CD679D; display: inline-block; cursor:pointer;" onClick="closePopup()">${weiterText} ${" "} ${
     site === "147" ? "147.ch" : "projuventute.ch"
