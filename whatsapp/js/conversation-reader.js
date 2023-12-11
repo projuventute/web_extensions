bbd6152984ecb29f08d23f913b6a7152877a92d1
pjdwe <david.welten@projuventute.ch>
2023-12-11 06:53:24 +0100

    Version 0.7

--- whatsapp/js/conversation-reader.js
+++ whatsapp/js/conversation-reader.js
@@ -3,6 +3,30 @@ function makeReadable() {
 
     if (!existingButton) {
         createToggleButton();
+        // Find the path element with the specified attribute
+        const pathElement = document.querySelector('path[d="M5.264 2C2.878 2 1.496 4.703 2.893 6.637L6.766 12l-3.873 5.363C1.496 19.297 2.878 22 5.263 22c.651 0 1.291-.163 1.862-.475l13.44-7.33c1.737-.948 1.737-3.442 0-4.39l-13.44-7.33A3.887 3.887 0 0 0 5.264 2Zm-.75 3.466A.925.925 0 0 1 5.264 4c.315 0 .626.08.903.23L18.578 11H8.511L4.514 5.466ZM8.511 13l-3.997 5.534A.925.925 0 0 0 5.264 20c.315 0 .626-.08.903-.23L18.578 13H8.511Z"]');
+
+        if (pathElement) {
+            // Find the nearest ancestor button element
+            const ancestorButton = pathElement.closest('button');
+
+            if (ancestorButton) {
+                // Add an event listener to the ancestorButton
+                ancestorButton.addEventListener('click', () => {
+                    console.log("TOGGLED");
+                    // Assuming isClean is a boolean variable
+                    if (isClean) {
+                        setTimeout(function(){ location.reload(); },500);
+                    }
+                });
+            } else {
+                console.log('No ancestor button found.');
+            }
+        } else {
+            console.log('Path element not found.');
+        }
+
+
     }
     checkElementsExistence('svg > circle', 20, 30000, cleanReader, true);
 }
@@ -18,15 +42,18 @@ function cleanReader() {
     if (cleanMessages.length > 0) {
         userMessages.forEach(message => {
             message.classList.add("extMessage");
+            message.classList.add("userMessage");
         });
         notesMessages.forEach(message => {
             message.classList.add("extMessage");
         })
         dayMessages.forEach(message => {
             message.classList.add("extMessage");
+            message.classList.add("dayMessage");
         });;
         ourMessages.forEach(message => {
             message.classList.add("extMessage");
+            message.classList.add("ourMessage");
         });
         allMessages.forEach(message => {
             if (!hasClass(message, "extMessage")) {
@@ -74,25 +101,30 @@ function findDivsWithComputedStyle(styleProperty, styleValue, tagName = 'div', w
 var cleanButton;
 // Function to create a toggle button and append it to the specified element if it doesn't already exist
 function createToggleButton() {
-    // Check if the button already exists within the specified element
+    // Find the path element with the specific "d" attribute
+    const path = document.querySelector('path[d^="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2s10 4.477 10 10Zm-2.062 1a8.008 8.008 0 0 1-5.216 6.525c.058-.108.116-.22.174-.337.717-1.443 1.42-3.505 1.573-6.188h3.47Zm0-2a8.009 8.009 0 0 0-5.162-6.505c.038.07.076.144.114.218.737 1.44 1.456 3.524 1.588 6.287h3.46Zm-5.462 0c-.13-2.41-.757-4.185-1.366-5.376A9.172 9.172 0 0 0 12.06 4h-.104a9.996 9.996 0 0 0-1.06 1.701c-.6 1.206-1.212 2.967-1.362 5.299h4.942Zm-4.952 2c.13 2.41.757 4.185 1.366 5.376A9.171 9.171 0 0 0 11.94 20h.104a9.997 9.997 0 0 0 1.06-1.701c.6-1.206 1.212-2.967 1.362-5.299H9.524Zm-2.002 0c.132 2.763.85 4.846 1.588 6.287l.114.218A8.009 8.009 0 0 1 4.062 13h3.46Zm.009-2c.153-2.683.856-4.745 1.573-6.188.058-.117.116-.229.174-.337A8.009 8.009 0 0 0 4.062 11H7.53Z"]');
+    if (!path) {
+        return;
+    }
+    // Traverse up to find the parent SVG and then the parent button
+    const parentSvg = path.closest('svg');
+    if (!parentSvg) {
+        return;
+    }
+    const parentButton = parentSvg.closest('button');
+    if (!parentButton) {
+        return;
+    }
 
-    // Create a button element
+    // Create a new button element
     cleanButton = document.createElement("button");
-
-    // Add an event listener to toggle between cleanReader and uncleanReader
     cleanButton.addEventListener("click", () => {
         toggleClean();
     });
-    cleanButton.id = "uncleaner"
-    // Style the button and position it in the top left corner
-    cleanButton.style.position = "relative";
-    cleanButton.style.top = window.innerHeight - 30 + "px";
-    cleanButton.style.left = "70px";
+    cleanButton.id = "uncleaner";
     cleanButton.innerHTML = '<svg width="2em" height="2em" viewBox="0 0 256 256" fill="none"><g xmlns="http://www.w3.org/2000/svg"><g><path fill="#000000" d="M168.5,250.6c-0.9,0-0.9,0-1.8,0c-3.6-0.9-8.1-0.9-11.7-1.8s-7.2-0.9-11.7-1.8c-2.7-0.9-6.3-0.9-9-1.8l-10.8-2.7l4.5-6.3c4.5-7.2,9.9-13.5,13.5-19.8c0.9-0.9,1.8-1.8,2.7-3.6c-2.7,1.8-6.3,4.5-9.9,7.2c-14.4,10.8-34.2,12.6-50.4,4.5l-13.5-6.3c-1.8-0.9-3.6-2.7-4.5-5.4c-0.9-2.7,0-4.5,1.8-7.2c3.6-3.6,6.3-7.2,8.1-9.9c-2.7,0.9-4.5,1.8-7.2,2.7c-12.6,4.5-26.1,1.8-36-6.3c-9.9-8.1-15.3-12.6-16.2-13.5l-6.3-6.3l9.9-1.8c0.9,0,78.4-19.8,123.4-73.9l2.7-2.7l87.4,38.7l-1.8,4.5c-4.5,13.5-21.6,61.2-54,109C175.7,247,173.9,250.6,168.5,250.6z M140.6,235.3c1.8,0,3.6,0.9,5.4,0.9c3.6,0.9,7.2,0.9,10.8,1.8c3.6,0.9,8.1,0.9,11.7,1.8c29.7-43.2,45.9-86.5,52.2-102.7l-72-32.4c-38.7,44.1-95.4,65.7-118,73c2.7,1.8,5.4,4.5,9,7.2c7.2,6.3,17.1,8.1,26.1,4.5c9.9-3.6,18-7.2,20.7-9.9l10.8-9.9l-2.7,14.4c-0.9,7.2-11.7,19.8-18,27l10.8,5.4c13.5,6.3,28.8,4.5,40.5-3.6c11.7-9,20.7-15.3,25.2-18.9c0.9-0.9,2.7-2.7,4.5-3.6l13.5-12.6l-3.6,15.3c-1.8,6.3-8.1,15.3-18.9,29.7C146.9,226.3,143.3,230.8,140.6,235.3z"/><path fill="#000000" d="M230.7,138l-87.4-38.7l1.8-4.5c2.7-6.3,6.3-10.8,12.6-13.5c9-5.4,18.9-6.3,27.9-1.8c10.8,4.5,20.7,9,31.5,14.4c14.4,6.3,21.6,21.6,17.1,36.9c0,0.9-0.9,1.8-0.9,2.7L230.7,138z M155.9,94.8l68.4,30.6c1.8-9.9-2.7-18.9-11.7-22.5c-9.9-4.5-20.7-9-31.5-13.5c-6.3-2.7-13.5-2.7-19.8,0.9C159.5,90.3,157.7,92.1,155.9,94.8z"/><path fill="#000000" d="M218.9,92.1l-4.5-1.8l-25.2-11.7l-3.6-3.6l2.7-5.4l30.6-57.6c2.7-5.4,6.3-7.2,12.6-6.3c4.5,0.9,9,2.7,11.7,6.3c2.7,2.7,3.6,7.2,1.8,10.8l-24.3,64.8L218.9,92.1z M197.3,72.3l16.2,7.2L235.2,20c0-0.9,0-0.9,0-0.9c-0.9-1.8-2.7-2.7-5.4-2.7c-0.9,0-1.8,0-1.8,0s0,0-0.9,0.9L197.3,72.3z"/><path fill="#000000" d="M216.2,85.8"/></g></g></svg>';
-
-
-    // Append the button to the specified element
-    document.body.appendChild(cleanButton);
+    // Replace the old button with the new button
+    parentButton.replaceWith(cleanButton);
 }
 
 function toggleClean(toggle = true) {
--- whatsapp/js/injectionHook.js
+++ whatsapp/js/injectionHook.js
@@ -2,57 +2,104 @@
  * @brief Function to set default settings.
  */
 function setDefaultSettings() {
-    // Use chrome.storage.sync.get to retrieve the settings from storage
-    chrome.storage.sync.get([
-        'media-block',
-        'conversation-reader'
-      ], function (data) {
-        // Check if settings exist in storage, if not, set default values
-        if (typeof data['media-block'] === 'undefined') {
-          chrome.storage.sync.set({
-            'media-block': true // Default value for media blocking
-          });
-        }
-        if (typeof data['conversation-reader'] === 'undefined') {
-          chrome.storage.sync.set({
-            'conversation-reader': true // Default value for media blocking
-          });
-        }
-        // Call the loadAndInject function after setting default settings
-        loadAndInject();
-    });
+  // Use chrome.storage.sync.get to retrieve the settings from storage
+  chrome.storage.sync.get([
+    'media-block',
+    'conversation-reader',
+    'extension-is-active'
+  ], function (data) {
+    console.log("isActive="+data['extension-is-active']);
+    // Check if settings exist in storage, if not, set default values
+    if (typeof data['media-block'] === 'undefined') {
+      chrome.storage.sync.set({
+        'media-block': true // Default value for media blocking
+      });
+    }
+    if (typeof data['conversation-reader'] === 'undefined') {
+      chrome.storage.sync.set({
+        'conversation-reader': true // Default value for media blocking
+      });
+    }
+    if (typeof data['extension-is-active'] === 'undefined') {
+      chrome.storage.sync.set({
+        'extension-is-active': true // Default value for media blocking
+      });
+    }
+    // Call the loadAndInject function after setting default settings
+    loadAndInject();
+  });
 }
 
 /**
  * @brief Call the setDefaultSettings function when the options page loads
  */
+
 setDefaultSettings();
+
 
 /**
  * @brief Function to load settings and inject scripts
  */
 function loadAndInject() {
-    // Load and make settings available to injected scripts
-    chrome.storage.sync.get([
-        'media-block',
-        'conversation-reader'
-    ], function (data) {
-        var settingsContainer = document.createElement('div');
-        settingsContainer.id = "settings-container";
-        data['extensionBase'] = chrome.runtime.getURL('/');
-        settingsContainer.innerHTML = JSON.stringify(data);
-        (document.body || document.documentElement).appendChild(settingsContainer);
+  // Load and make settings available to injected scripts
+  chrome.storage.sync.get([
+    'media-block',
+    'conversation-reader',
+    'dark-mode',
+    'conversation-preview',
+    'hide-admins',
+    'not-delivered',
+    'clean-layout',
+    'beraitung-url',
+    'beraitung-sig',
+    'extension-is-active'
+  ], function (data) {
+    if (data['extension-is-active']) {
+      var settingsContainer = document.createElement('div');
+      settingsContainer.id = "settings-container";
+      data['extensionBase'] = chrome.runtime.getURL('/');
+      data['ll_show_unanswered'] = chrome.i18n.getMessage("ll_show_unanswered");
+      data['ll_show_all'] = chrome.i18n.getMessage("ll_show_all");
+      settingsContainer.innerHTML = JSON.stringify(data);
+      (document.body || document.documentElement).appendChild(settingsContainer);
 
-        // Load the injected scripts
-        var s = document.createElement('script');
-        s.src = chrome.runtime.getURL('js/injected.js');
-        (document.head || document.documentElement).appendChild(s);
+      // Load the injected scripts
+      var s = document.createElement('script');
+      s.src = chrome.runtime.getURL('js/injected.js');
+      (document.head || document.documentElement).appendChild(s);
 
+      var s = document.createElement('script');
+      s.src = chrome.runtime.getURL('js/media-block.js');
+      (document.head || document.documentElement).appendChild(s);
+      var s = document.createElement('script');
+      s.src = chrome.runtime.getURL('js/conversation-reader.js');
+      (document.head || document.documentElement).appendChild(s);
+      if (data['dark-mode']) {
         var s = document.createElement('script');
-        s.src = chrome.runtime.getURL('js/media-block.js');
+        s.src = chrome.runtime.getURL('js/darkmode.js');
         (document.head || document.documentElement).appendChild(s);
+      }
+      if (data['conversation-preview']) {
         var s = document.createElement('script');
-        s.src = chrome.runtime.getURL('js/conversation-reader.js');
-        (document.head || document.documentElement).appendChild(s);        
-    });
+        s.src = chrome.runtime.getURL('js/conversation-preview.js');
+        (document.head || document.documentElement).appendChild(s);
+        var iframeContainer = document.createElement('div');
+        iframeContainer.id = "iframe-container";
+        iframeContainer.innerHTML = '<iframe id="my-iframe" src="" frameborder="0" width="100%" height="100%"></iframe>';
+        (document.body || document.documentElement).appendChild(iframeContainer);
+
+      }
+      if (data['clean-layout']) {
+        var s = document.createElement('script');
+        s.src = chrome.runtime.getURL('js/clean-layout.js');
+        (document.head || document.documentElement).appendChild(s);
+      }
+      if (data['beraitung-url']) {
+        var s = document.createElement('script');
+        s.src = chrome.runtime.getURL('js/beraitung.js');
+        (document.head || document.documentElement).appendChild(s);
+      }
+    }
+  });
+
 }
