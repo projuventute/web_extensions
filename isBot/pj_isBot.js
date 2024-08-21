bc28c0d1f6c764dee48fd66642de372f067f90ef
Martin Seifert <103060395+martinseifertprojuventute@users.noreply.github.com>
2024-08-21 09:30:13 +0200

    added optional domain

--- isBot/pj_isBot.js
+++ isBot/pj_isBot.js
@@ -36,7 +36,7 @@
                 return
             }
             domain = domain || ""; // defaul value of empty string
-            if (domain !== "") {
+            if (domain !== '') {
                 domain = '; domain=' + domain + ';';
             }
             // write cookie
@@ -47,18 +47,18 @@
         }
     }
 
-    function identifyBot() {
+    function identifyBot(domain) {
         try {
             var cookieValue = pj_isBot.cookieRead('pj_bot');
             if(typeof cookieValue === "undefined") {
                 var ua = navigator.userAgent;
                 var fullPattern = new RegExp("bot|spider|crawl|http|lighthouse| daum[ /]| deusu/| yadirectfetcher|(?:^|[^g])news(?!sapphire)|(?<! (?:channel/|google/))google(?!(app|/google| pixel))|(?<! cu)bots?(?:\\b|_)|(?<!(?:lib))http|(?<![hg]m)score|@[a-z][\\w-]+\\.|\\(\\)|\\.com|\\b\\d{13}\\b|^<|^[\\w \\.\\-\\(?:\\):]+(?:/v?\\d+(?:\\.\\d+)?(?:\\.\\d{1,10})*?)?(?:,|$)|^[^ ]{50,}$|^\\d+\\b|^\\w*search\\b|^\\w+/[\\w\\(\\)]*$|^active|^ad muncher|^amaya|^avsdevicesdk/|^biglotron|^bot|^bw/|^clamav[ /]|^client/|^cobweb/|^custom|^ddg[_-]android|^discourse|^dispatch/\\d|^downcast/|^duckduckgo|^facebook|^getright/|^gozilla/|^hobbit|^hotzonu|^hwcdn/|^jeode/|^jetty/|^jigsaw|^microsoft bits|^movabletype|^mozilla/\\d\\.\\d \\(compatible;?\\)$|^mozilla/\\d\\.\\d \\w*$|^navermailapp|^netsurf|^offline|^owler|^php|^postman|^python|^rank|^read|^reed|^rest|^rss|^snapchat|^space bison|^svn|^swcd |^taringa|^thumbor/|^track|^valid|^w3c|^webbandit/|^webcopier|^wget|^whatsapp|^wordpress|^xenu link sleuth|^yahoo|^yandex|^zdm/\\d|^zoom marketplace/|^\{\{.*\}\}$|analyzer|archive|ask jeeves/teoma|bit\\.ly/|bluecoat drtr|browsex|burpcollaborator|capture|catch|check\\b|checker|chrome-lighthouse|chromeframe|classifier|cloudflare|convertify|crawl|cypress/|dareboost|datanyze|dejaclick|detect|dmbrowser|download|evc-batch/|exaleadcloudview|feed|firephp|functionize|gomezagent|headless|httrack|hubspot marketing grader|hydra|ibisbrowser|images|infrawatch|insight|inspect|iplabel|ips-agent|java(?!;)|library|linkcheck|mail\\.ru/|manager|measure|neustar wpm|node|nutch|offbyone|optimize|pageburst|pagespeed|parser|perl|phantomjs|pingdom|powermarks|preview|proxy|ptst[ /]\\d|reputation|resolver|retriever|rexx;|rigor|rss\\b|scan|scrape|server|sogou|sparkler/|speedcurve|spider|splash|statuscake|supercleaner|synapse|synthetic|tools|torrent|trace|transcoder|url|virtuoso|wappalyzer|webglance|webkit2png|whatcms/|zgrab", "i");
-            
+                domain = domain || ''; // defaul value of empty string
                 if (fullPattern.test(ua)) {
-                    pj_isBot.cookieCreate('pj_bot', 'true', '');
+                    pj_isBot.cookieCreate('pj_bot', 'true', domain);
                     return true;
                 } else {
-                    pj_isBot.cookieCreate('pj_bot', 'false', '');
+                    pj_isBot.cookieCreate('pj_bot', 'false', domain);
                     return false;
                 }
             } else {
@@ -71,3 +71,5 @@
     }
 
 })();
+
+pj_isBot.identifyBot('.projuventute.ch');
